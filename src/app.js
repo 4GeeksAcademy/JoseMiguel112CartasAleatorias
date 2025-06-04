9e1d7b6e91a23dbea03e7c614931cda170f167ea
Jose <jm.bebor@gmail.com>
2025-06-04 11:02:25 +0000

    [Feature]Random logic made, lack of card, lack of connection with render

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function randomvalues(array){
+  return array[Math.floor(Math.random()*array.length)]
+}
+
+   
+class Palo{
+  constructor(simbolo, color){
+    this.simbolo=simbolo
+    this.color=color
+  }
+}
+const corazones=new Palo("♥","danger");
+const diamantes=new Palo("♦","danger");
+const picas=new Palo("♠","dark");
+const treboles=new Palo("♣", "dark");
+
+const palos=[corazones, diamantes, picas, treboles];
+
+class Card{
+  constructor(palo,valor){
+    this.palo=palo;
+    this.valor=valor
+  }
+}
+
+
+const valores=["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+
+
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+
+  const rcard=new Card(randomvalues(palos), randomvalues(valores))
+  console.log(rcard)
 };
+
+
